5c8e0c803491018cb9321339cb099e4a5d59ae87
bastyz <bastian.inostroza@gmail.com>
2018-04-23 21:36:58 -0300

    added end of line

--- subasta.c
+++ subasta.c
@@ -28,18 +28,18 @@ typedef struct subasta {
 // otras funciones que necesite.
 
 Subasta nuevaSubasta(int unidades){
-    nPrintf("entra a subasta");
+    nPrintf("entra a subasta\n");
     Subasta subasta = nMalloc(sizeof(*subasta));
-    nPrintf("asigna espacio a la subasta");
+    nPrintf("asigna espacio a la subasta\n");
     subasta->monitor = nMakeMonitor();
     subasta->finalizado = 0;
     subasta->unidades = unidades;
     subasta->count = 0;
     subasta->indexMin = 0;
-    nPrintf("inicia arreglo");
+    nPrintf("inicia arreglo\n");
     Postor postor1 = nMalloc(sizeof(*postor1));
     subasta->postor[unidades] = nMalloc(unidades*sizeof(*postor1));
-    nPrintf("termina arreglo");
+    nPrintf("termina arreglo\n");
     return subasta;
 }
 
