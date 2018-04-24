83afbf8fc951c32f533e73fc21be5d4750cf3023
BastyZ <bastian.inostroza@gmail.com>
2018-04-23 21:54:21 -0300

    debug ofrecer para la segunda opción (subasta no llena ni vacía) 3.6.1

--- subasta.c
+++ subasta.c
@@ -97,7 +97,7 @@ int ofrecer(Subasta s, double precio){
                     nPrintf("    Comienzo a buscar el menor\n");
                     Postor *aux = s->postor[i];
                     nPrintf("    Creé un auxiliar y comparo\n");
-                    nPrintf("    El precio del auxiliar es: %d\n", *aux->precio);
+                    nPrintf("    El precio del auxiliar es: %d\n", *aux.precio);
                     if(*aux->precio < s->min){
                         nPrintf("    Los cambio\n");
                         s->min = *aux->precio;
