c9bf9ffe824d74dacd1c5d9fd4490b2ad85705c2
BastyZ <bastian.inostroza@gmail.com>
2018-04-23 21:32:47 -0300

    inicio de print debug

--- subasta.c
+++ subasta.c
@@ -28,14 +28,18 @@ typedef struct subasta {
 // otras funciones que necesite.
 
 Subasta nuevaSubasta(int unidades){
+    nPrintf("entra a subasta");
     Subasta subasta = nMalloc(sizeof(*subasta));
+    nPrintf("asigna espacio a la subasta");
     subasta->monitor = nMakeMonitor();
     subasta->finalizado = 0;
     subasta->unidades = unidades;
     subasta->count = 0;
     subasta->indexMin = 0;
+    nPrintf("inicia arreglo");
     Postor postor1 = nMalloc(sizeof(*postor1));
     subasta->postor[unidades] = nMalloc(unidades*sizeof(*postor1));
+    nPrintf("termina arreglo");
     return subasta;
 }
 
